16ab104a354ad0b7ce8cb39f13c64ae25e9ab68a
Rhody Kaner <rhodykaner@c027h061.wless.reed.edu>
2018-09-19 14:15:07 -0700

    Thesis code

---
+++ Thesiscode.r
@@ -0,0 +1,70 @@
+#TO DO:
+# get names of authors into lists by conference
+#for each conference:
+# read through each list of authors and find their gender in the gender lists 
+# - create counts of women, men, and N/A, graph line
+#graph fraction of women?
+
+#Reading in JSON files
+
+library(jsonlite)
+library(dplyr)
+library(stringr)
+
+inf_gender_data <- fromJSON("inferred_gender_mapping.json")
+ver_gender_data <- fromJSON("verified_gender_mapping.json")
+gender_data <- rbind(inf_gender_data, ver_gender_data)
+
+#COMBINE GENDER DATA
+
+#loop to get PC chairs by conference
+conf_files <- list.files("conf")
+counter = 0
+df_pc <- data.frame(conf = c(), total_pc_chairs = c(), women_pc_chairs = c(), ratio_women_pc_chairs = c())
+
+for (file in conf_files) {
+    #grab a conference
+    conference <- fromJSON(toString(file))
+    
+    pc_chair_list = conference$pc_chairs
+
+    total = 0
+    women_total = 0
+    #counting up the totals
+    for (person in pc_chair_list) {
+    	
+        gender = gender_data[person]
+            
+        if (gender != "N/A") {
+        	if (gender == "F") {
+            	women_total = women_total + 1
+        	total = total + 1
+        	}
+        }
+    }
+           
+    ratio = women_total/total
+    
+    #add values to data frame
+    rbind(df_pc, conf = conference$key, total_pc_chairs = total, women_pc_chairs = women_total, ratio_women_pc_chairs = ratio)
+}
+
+pc_data <- table(df_pc$ratio_women_pc_chairs)
+
+barplot(pc_data, main = "PC Chair Gender Distribution", horiz = TRUE, names.arg=conf)
+
+#distilling data for graph
+#for each file in author_data, grab name (first variable) and make list of those
+#MAKE EVERYTHING DATAFRAMES < NOT LISTS 
+#df <- data.frame(conf = c(), total = c(), female=c()) ---> sets columns
+#for ...
+#    rbind(df, conf=,  )
+#df$conf <- list_of_conf_names
+
+#creating graph
+
+# Simple Horizontal Bar Plot with Added Labels 
+#counts <- table(mtcars$gear)
+#barplot(counts, main="Car Distribution", horiz=TRUE,
+#  names.arg=c("3 Gears", "4 Gears", "5 Gears"))
+
